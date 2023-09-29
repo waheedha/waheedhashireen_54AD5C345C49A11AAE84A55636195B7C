404bb856268c136a3d1f2ce6948fd99dac667217
waheedha <146366915+waheedha@users.noreply.github.com>
2023-09-29 19:04:20 +0530

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,11 @@
+def isleapyear(year):
+  if(year % 4 ==0 and year % 100 !=0) or year % 400 ==0:
+    return True
+  else:
+    return False
+
+year=int(input("Enter the year : "))
+if isleapyear(year):
+  print('{} is a Leap Year.'.format(year))
+else:
+  print('{} is not a Leap Year.'.format(year))
---
+++ main (2).py
@@ -0,0 +1,9 @@
+def factp(n):
+  if n==0 or n==1:
+    return 1
+  else:
+    return n*factp(n-1)
+
+num=int(input("Enter the Value :"))
+result = factp(num)
+print("The {}! value is {}".format(num,result))
